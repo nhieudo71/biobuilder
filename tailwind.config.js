95e882ae7bfc3e45d40d5dac4caedede84d76797
nhieudo71 <80392418+nhieudo71@users.noreply.github.com>
2026-04-01 10:36:51 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+        mono: ['"Fira Code"', 'monospace'],
+        retro: ['"Space Grotesk"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
